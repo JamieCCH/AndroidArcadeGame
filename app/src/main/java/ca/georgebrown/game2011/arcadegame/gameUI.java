e0cdae08d7f44e3db7bef13f8a9fbd1c74dc9a62
JamieCCH <jamie.cch@gmail.com>
2018-04-11 14:27:52 -0400

    Roughly done the framework, run under 1920x1200

--- app/src/main/java/ca/georgebrown/game2011/arcadegame/gameUI.java
+++ app/src/main/java/ca/georgebrown/game2011/arcadegame/gameUI.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 /**
@@ -11,12 +12,20 @@ import android.view.View;
  */
 
 class gameUI extends View {
-    Bitmap bgr, scoreBgr, timerBgr, heartFull, heartEmpty, bombFull, bombEmpty;
+
+    Bitmap bgr, scoreBgr, timerBgr, heartFull, heartEmpty, bombFull, bombEmpty, bombButton, pauseButton;
 
     public gameUI(Context context) {
         super(context);
-        bgr = BitmapFactory.decodeResource(getResources(),R.drawable.background_gameplay);
-        scoreBgr = BitmapFactory.decodeResource(getResources(),R.drawable.hud_score);
+        bgr = BitmapFactory.decodeResource(getResources(),R.mipmap.background_gameplay);
+        scoreBgr = BitmapFactory.decodeResource(getResources(),R.mipmap.hud_score);
+        bombButton = BitmapFactory.decodeResource(getResources(),R.mipmap.button_bomb);
+        timerBgr = BitmapFactory.decodeResource(getResources(),R.mipmap.hud_timer);
+        pauseButton = BitmapFactory.decodeResource(getResources(),R.mipmap.button_pause);
+        heartFull = BitmapFactory.decodeResource(getResources(),R.mipmap.hud_fullheart);
+        heartEmpty = BitmapFactory.decodeResource(getResources(),R.mipmap.hud_emptyheart);
+        bombFull = BitmapFactory.decodeResource(getResources(),R.mipmap.hud_fullbomb);
+        bombEmpty = BitmapFactory.decodeResource(getResources(),R.mipmap.hud_emptybomb);
     }
 
     public void onDraw(Canvas canvas){
@@ -24,5 +33,20 @@ class gameUI extends View {
 
         canvas.drawBitmap(bgr, 0, 0, null);
         canvas.drawBitmap(scoreBgr,10,10,null);
+        canvas.drawBitmap(bombButton, 400, 20,null);
+        canvas.drawBitmap(timerBgr, 500, 25, null);
+        canvas.drawBitmap(pauseButton, 500, 20, null);
+
+        //make an array later
+        canvas.drawBitmap(heartFull, 15, 100, null);
+        canvas.drawBitmap(heartFull, 60, 100, null);
+        canvas.drawBitmap(heartEmpty, 105, 100, null);
+
+        //make an array as well
+        canvas.drawBitmap(bombFull, 180, 90, null);
+        canvas.drawBitmap(bombFull, 220, 90, null);
+        canvas.drawBitmap(bombEmpty, 260, 90, null);
+        canvas.drawBitmap(bombEmpty, 300, 90, null);
+        canvas.drawBitmap(bombEmpty, 340, 90, null);
     }
 }
