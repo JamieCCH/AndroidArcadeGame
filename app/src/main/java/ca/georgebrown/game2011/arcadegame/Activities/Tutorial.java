cf5769cf0e13e41bc5011dd4ddda186b23149fbe
JamieCCH <jamie.cch@gmail.com>
2018-04-23 04:47:20 -0400

    Add tutorial and credit screens

--- app/src/main/java/ca/georgebrown/game2011/arcadegame/Activities/Tutorial.java
+++ app/src/main/java/ca/georgebrown/game2011/arcadegame/Activities/Tutorial.java
@@ -1,7 +1,11 @@
 package ca.georgebrown.game2011.arcadegame.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import ca.georgebrown.game2011.arcadegame.R;
 
 public class Tutorial extends Activity {
@@ -10,5 +14,32 @@ public class Tutorial extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+        ImageButton play = findViewById(R.id.startButton);
+        play.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startPlay();
+            }
+        });
+
+        ImageButton back = findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                menu();
+            }
+        });
     }
+
+    private void startPlay() {
+
+        Intent intent = new Intent(this, GamePlayActivity.class);
+        startActivity(intent);
+    }
+
+    private void menu() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
